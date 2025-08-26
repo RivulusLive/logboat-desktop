ddcfde889e622e97da777414908a2bd75e430241
nekename <63245705+nekename@users.noreply.github.com>
2025-08-26 22:17:59 +0100

    chore(logboat): disable star and sponsorship prompt

--- src/components/SettingsView.svelte
+++ src/components/SettingsView.svelte
@@ -158,6 +158,7 @@
 		<span class="text-xs dark:text-neutral-400">
 			{@html buildInfo}
 		</span>
+		<!--
 		<div class="absolute bottom-6 flex flex-row items-center text-sm dark:text-neutral-400">
 			<span class="mr-1">
 				Please leave a
@@ -171,5 +172,6 @@
 			<Heart weight="fill" fill="fuchsia" />
 			<span class="ml-1">for my work :)</span>
 		</div>
+		-->
 	</div>
 </Popup>
